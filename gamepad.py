47659ed06a2da0eb7255d4852aebdaf0c86ef789
bohdaholas <73237406+bohdaholas@users.noreply.github.com>
2021-05-08 00:43:11 +0300

    Update gamepad.py

--- gamepad.py
+++ gamepad.py
@@ -24,48 +24,48 @@ class Gamepad:
     DPadY: int = 0
 
     def read_data(self, verbose=False):
-        while True:
-            for event in get_gamepad():
-                if event.ev_type == "Key":  # category of binary respond values
-                    if event.code == "BTN_BASE5":
-                        self.LStickBtn = event.state
-                    elif event.code == "BTN_BASE6":
-                        self.RStickBtn = event.state
-                    elif event.code == "BTN_TRIGGER":
-                        self.triangle = event.state
-                    elif event.code == "BTN_THUMB":
-                        self.circle = event.state
-                    elif event.code == "BTN_THUMB2":
-                        self.cross = event.state
-                    elif event.code == "BTN_TOP":
-                        self.square = event.state
-                    elif event.code == "BTN_BASE":
-                        self.LBumper = event.state
-                    elif event.code == "BTN_BASE2":
-                        self.RBumper = event.state
-                    elif event.code == "BTN_TOP2":
-                        self.LTrigger = event.state
-                    elif event.code == "BTN_PINKIE":
-                        self.RTrigger = event.state
-                    elif event.code == "BTN_BASE3":
-                        self.select = event.state
-                    elif event.code == "BTN_BASE4":
-                        self.start = event.state
-                elif event.ev_type == "Absolute":  # category of analog respond values
-                    if event.code == "ABS_Y":
-                        self.LStickY = event.state
-                    elif event.code == "ABS_X":
-                        self.LStickX = event.state
-                    elif event.code == "ABS_Z":
-                        self.RStickY = event.state
-                    elif event.code == "ABS_RZ":
-                        self.RStickX = event.state
-                    elif event.code == "ABS_HAT0Y":
-                        self.DPadX = event.state
-                    elif event.code == "ABS_HAT0X":
-                        self.DPadY = event.state
-            if verbose:
-                print(self)
+        for event in get_gamepad():
+            if event.ev_type == "Key":  # category of binary respond values
+                if event.code == "BTN_BASE5":
+                    self.LStickBtn = event.state
+                elif event.code == "BTN_BASE6":
+                    self.RStickBtn = event.state
+                elif event.code == "BTN_TRIGGER":
+                    self.triangle = event.state
+                elif event.code == "BTN_THUMB":
+                    self.circle = event.state
+                elif event.code == "BTN_THUMB2":
+                    self.cross = event.state
+                elif event.code == "BTN_TOP":
+                    self.square = event.state
+                elif event.code == "BTN_BASE":
+                    self.LBumper = event.state
+                elif event.code == "BTN_BASE2":
+                    self.RBumper = event.state
+                elif event.code == "BTN_TOP2":
+                    self.LTrigger = event.state
+                elif event.code == "BTN_PINKIE":
+                    self.RTrigger = event.state
+                elif event.code == "BTN_BASE3":
+                    self.select = event.state
+                elif event.code == "BTN_BASE4":
+                    self.start = event.state
+            elif event.ev_type == "Absolute":  # category of analog respond values
+                if event.code == "ABS_Y":
+                    self.LStickY = event.state
+                elif event.code == "ABS_X":
+                    self.LStickX = event.state
+                elif event.code == "ABS_Z":
+                    self.RStickY = event.state
+                elif event.code == "ABS_RZ":
+                    self.RStickX = event.state
+                elif event.code == "ABS_HAT0Y":
+                    self.DPadX = event.state
+                elif event.code == "ABS_HAT0X":
+                    self.DPadY = event.state
+        if verbose:
+            print(self)
+        return self
 
 
 if __name__ == '__main__':
